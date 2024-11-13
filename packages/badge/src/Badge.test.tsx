c9ac9d6bb867c6d94886fa6f6a78618ce98ae684
Euihyun Yang <euihyun.yang.9x@gmail.com>
2024-11-13 23:12:20 +0900

    feat(badge): add test scenario

---
+++ packages/badge/src/Badge.test.tsx
@@ -0,0 +1,19 @@
+import { test } from 'vitest';
+
+test.todo('children으로 입력한 텍스트를 표시한다.');
+
+test.todo('모서리가 8px radius 형태이다.');
+
+test.todo('color는 teal(#00FFFF) 색상이다.');
+
+test.todo('variant를 주입하지 않으면 filled를 기본 형태로 설정한다.');
+
+test.todo('variant가 weak인 경우');
+
+test.todo('variant가 outline인 경우');
+
+test.todo('size를 주입하지 않으면 medium을 기본 크기로 설정한다.');
+
+test.todo('size가 small인 경우');
+
+test.todo('size가 large인 경우');
