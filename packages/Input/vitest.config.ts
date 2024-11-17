5a994e5e7e7d51a06e8498b056b9547d02ebdbbc
Jeongmin Lee <clcl6084@gmail.com>
2024-11-17 20:53:45 +0900

    test(input): setup and install jest and rtl related packages

--- packages/Input/vitest.config.ts
+++ packages/Input/vitest.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    globals: true,
     passWithNoTests: true,
+    setupFiles: './vitest.setup.ts',
     watch: false,
+    css: true,
   },
 });
