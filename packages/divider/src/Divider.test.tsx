503533a11361217b13f99b8903a6fee6d5ae8a45
limhizy15 <limhizy15@ajou.ac.kr>
2024-11-20 15:00:23 +0900

    feat(divider): add test code and implementation

--- packages/divider/src/Divider.test.tsx
+++ packages/divider/src/Divider.test.tsx
@@ -1,5 +1,40 @@
-import { test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { Divider } from './Divider';
+import styles from './divider.module.css';
 
-test.todo('orientation 속성이 없으면 가로 방향으로 그린다.');
-test.todo('orientation이 vertical이면 세로 방향으로 그린다.');
-test.todo('전달받은 style 속성이 컴포넌트에 적용된다.');
+describe('Divider', () => {
+  test('orientation 속성이 없으면 가로 방향으로 그린다.', () => {
+    render(<Divider />);
+
+    const divider = screen.getByRole('separator');
+
+    expect(divider).toHaveAttribute('aria-orientation', 'horizontal');
+    expect(divider).toHaveClass(styles.horizontal);
+  });
+
+  test('orientation이 vertical이면 세로 방향으로 그린다.', () => {
+    render(<Divider orientation="vertical" />);
+
+    const divider = screen.getByRole('separator');
+
+    expect(divider).toHaveAttribute('aria-orientation', 'vertical');
+    expect(divider).toHaveClass(styles.vertical);
+  });
+
+  test('전달받은 style 속성이 컴포넌트에 적용된다.', () => {
+    const testStyle = {
+      backgroundColor: 'red',
+      margin: '8px',
+    };
+
+    render(<Divider style={testStyle} />);
+
+    const divider = screen.getByRole('separator');
+
+    expect(divider).toHaveStyle({
+      backgroundColor: 'red',
+      margin: '8px',
+    });
+  });
+});
--- packages/divider/src/Divider.tsx
+++ packages/divider/src/Divider.tsx
@@ -1,3 +1,17 @@
-export function Divider() {
-  return <div>Component</div>;
+import type { CSSProperties } from 'react';
+import styles from './Divider.module.css';
+
+interface DividerProps {
+  orientation?: 'horizontal' | 'vertical';
+  style?: CSSProperties;
+}
+
+export function Divider({ orientation = 'horizontal', style }: DividerProps) {
+  return (
+    <hr
+      aria-orientation={orientation}
+      className={`${styles[orientation]} ${styles.divider}`}
+      style={style}
+    />
+  );
 }
