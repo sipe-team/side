fd98764004fd83e644a59ecedcb539edd2345cd3
Euihyun Yang <euihyun.yang.9x@gmail.com>
2024-12-01 17:47:36 +0900

    feat(tokens): add tokens package

---
+++ packages/tokens/src/fonts.ts
@@ -0,0 +1,24 @@
+export const fontSize = {
+  12: 12,
+  14: 14,
+  16: 16,
+  18: 18,
+  20: 20,
+  24: 24,
+  28: 28,
+  32: 32,
+  36: 36,
+  48: 48,
+} as const;
+
+export const fontWeight = {
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+} as const;
+
+export const lineHeight = {
+  regular: 1.5,
+  compact: 1.3,
+} as const;
