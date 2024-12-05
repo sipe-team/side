8844a3f4d6f0f1f82aeb4baac993c16deb20bc2d
limhizy15 <limhizy15@ajou.ac.kr>
2024-12-05 14:33:29 +0900

    feat(radio-group): 스토리북 작성

--- packages/radio-group/src/RadioGroup.stories.tsx
+++ packages/radio-group/src/RadioGroup.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Radio, RadioGroup } from '.';
+import { useState } from 'react';
+import { Radio } from './Radio';
+import { RadioGroup } from './RadioGroup';
 
 const meta = {
   component: RadioGroup,
@@ -7,40 +9,77 @@ const meta = {
     layout: 'centered',
   },
 } satisfies Meta<typeof RadioGroup>;
-export default meta;
 
+export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {
-  args: {
-    value: 'option1',
-    onChangeValue: () => {},
-  },
-  render(args) {
+export const Default: Story = {
+  render: () => (
+    <RadioGroup labelText="과일" defaultValue="orange">
+      <Radio value="apple">사과</Radio>
+      <Radio value="orange">오렌지</Radio>
+      <Radio value="grape">포도</Radio>
+    </RadioGroup>
+  ),
+};
+
+export const Size: Story = {
+  render: () => (
+    <div>
+      <RadioGroup labelText="Small" size="small">
+        <Radio value="1">옵션 1</Radio>
+        <Radio value="2">옵션 2</Radio>
+      </RadioGroup>
+
+      <RadioGroup labelText="Medium" size="medium">
+        <Radio value="1">옵션 1</Radio>
+        <Radio value="2">옵션 2</Radio>
+      </RadioGroup>
+
+      <RadioGroup labelText="Large" size="large">
+        <Radio value="1">옵션 1</Radio>
+        <Radio value="2">옵션 2</Radio>
+      </RadioGroup>
+    </div>
+  ),
+};
+
+// 비활성화 상태
+export const Disabled: Story = {
+  render: () => (
+    <div className="space-y-4">
+      <RadioGroup labelText="전체 비활성화" disabled>
+        <Radio value="1">옵션 1</Radio>
+        <Radio value="2">옵션 2</Radio>
+      </RadioGroup>
+
+      <RadioGroup labelText="개별 비활성화">
+        <Radio value="1">활성화 옵션</Radio>
+        <Radio value="2" disabled>
+          비활성화 옵션
+        </Radio>
+      </RadioGroup>
+    </div>
+  ),
+};
+
+export const Controlled: Story = {
+  render: () => {
+    const [selectedValue, setSelectedValue] = useState('option1');
+
     return (
-      <>
-        <RadioGroup size={'large'}>
-          <Radio value={'apple'}>사과</Radio>
-          <Radio value={'orange'}>오렌지</Radio>
-          <Radio value={'grape'}>포도</Radio>
+      <div>
+        <RadioGroup
+          labelText="제어 컴포넌트"
+          value={selectedValue}
+          onChangeValue={setSelectedValue}
+        >
+          <Radio value="option1">옵션 1</Radio>
+          <Radio value="option2">옵션 2</Radio>
+          <Radio value="option3">옵션 3</Radio>
         </RadioGroup>
-
-        <RadioGroup disabled name="fruits" onChangeValue={() => {}}>
-          <Radio value="apple">사과</Radio>
-          <Radio value="orange">오렌지</Radio>
-        </RadioGroup>
-
-        <RadioGroup labelText="연락 방법" name="contact" size="large">
-          <Radio value="EMAIL" defaultChecked>
-            이메일
-          </Radio>
-          <Radio value="PHONE">전화</Radio>
-          <Radio value="FAX">팩스</Radio>
-          <Radio value="MAIL" disabled>
-            우편
-          </Radio>
-        </RadioGroup>
-      </>
+        <p>선택된 값: {selectedValue}</p>
+      </div>
     );
   },
 };
