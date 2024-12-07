9e7ebefc6258df4907b081b9e2548301a4e015f6
Jeongmin Lee <clcl6084@gmail.com>
2024-12-07 15:59:41 +0900

    feat(card): Card 테스트 코드 작성

--- packages/card/src/Card.test.tsx
+++ packages/card/src/Card.test.tsx
@@ -1,85 +1,50 @@
+import { color } from '@sipe-team/tokens';
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
-import { Badge } from './Card';
+import { Card } from './Card';
 
-test('children으로 입력한 텍스트를 표시한다.', () => {
-  render(<Badge>테스트</Badge>);
+test('children으로 넘어간 요소를 반환한다. ', () => {
+  render(
+    <Card>
+      <span>Card</span>
+    </Card>,
+  );
 
-  expect(screen.getByText('테스트')).toBeInTheDocument();
+  expect(screen.getByText('Card')).toBeInTheDocument();
 });
 
-test('모서리가 8px radius 형태이다.', () => {
-  render(<Badge>테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({ borderRadius: '8px' });
+test('Card는 기본적으로 padding(20px)을 갖는다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({ padding: '20px' });
 });
 
-test('글꼴 색상은 teal(#00FFFF)이다.', () => {
-  render(<Badge>테스트</Badge>);
-
-  expect(screen.getByText('테스트')).toHaveStyle({ color: '#00FFFF' });
-});
-
-test('글꼴 두께는 semiBold(600)이다.', () => {
-  render(<Badge>테스트</Badge>);
-
-  expect(screen.getByText('테스트')).toHaveStyle({ fontWeight: 600 });
-});
-
-test('variant를 주입하지 않으면 filled(배경색 #2D3748)를 기본 형태로 설정한다.', () => {
-  render(<Badge>테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({
-    backgroundColor: '#2D3748',
+test('Card는 기본적으로 요소를 중앙정렬을 한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   });
 });
 
-test('variant가 weak인 경우 배경색 #EDF2F7로 형태를 적용한다.', () => {
-  render(<Badge variant="weak">테스트</Badge>);
+test('ratio가 default로 aspect-ratio는 16/9로 반환한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle('aspect-ratio: 16 / 9');
+});
 
-  expect(screen.getByRole('status')).toHaveStyle({
-    backgroundColor: '#EDF2F7',
+test('ratio에 wide로 넣으면 aspect-ratio는 21/9로 반환한다.', () => {
+  render(<Card ratio="wide">Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle('aspect-ratio: 21 / 9');
+});
+
+test('variant는 default로 filled를 적용한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({
+    border: `1px solid ${color.gray200}`,
   });
 });
 
-test('variant가 outline인 경우 배경색은 투명, 테두리는 2px 두께의 #2D3748 색상 형태를 적용한다.', () => {
-  render(<Badge variant="outline">테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({
-    backgroundColor: 'transparent',
-    border: '2px solid #2D3748',
-  });
-});
-
-test('size를 주입하지 않으면 medium(상하 패딩 8px, 좌우 패딩 16px)을 기본 크기로 설정한다.', () => {
-  render(<Badge>테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({
-    paddingTop: '8px',
-    paddingBottom: '8px',
-    paddingLeft: '16px',
-    paddingRight: '16px',
-  });
-});
-
-test('size가 small인 경우 상하 패딩 4px, 좌우 패딩 8px 형태를 적용한다.', () => {
-  render(<Badge size="small">테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({
-    paddingTop: '4px',
-    paddingBottom: '4px',
-    paddingLeft: '8px',
-    paddingRight: '8px',
-  });
-});
-
-test('size가 large인 경우 상하 패딩 12px, 좌우 패딩 24px 형태를 적용한다.', () => {
-  render(<Badge size="large">테스트</Badge>);
-
-  expect(screen.getByRole('status')).toHaveStyle({
-    paddingTop: '12px',
-    paddingBottom: '12px',
-    paddingLeft: '24px',
-    paddingRight: '24px',
-  });
+test('variant가 outline으로 넣으면 border(#00FFFF) 색상을 적용한다.', () => {
+  render(<Card variant="outline">Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({ border: '1px solid #00FFFF' });
 });
