14bab7ca57c3326b18c013cb3d9be782a0b6fcd0
synuns <identityjang@gmail.com>
2024-12-15 21:44:40 +0900

    test(switch): enhance Switch component tests with additional scenarios
    - Expanded test suite for the Switch component to cover various states including defaultChecked, checked, and disabled.
    - Added tests to verify the correct rendering of the Switch based on props and to ensure event handling behaves as expected.

--- packages/switch/src/Switch.test.tsx
+++ packages/switch/src/Switch.test.tsx
@@ -1,15 +1,55 @@
-import { test } from 'vitest';
+import '@testing-library/jest-dom';
+import { faker } from '@faker-js/faker';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createRef } from 'react';
+import { describe, expect, test, vi } from 'vitest';
+import { Switch } from './Switch';
 
-test.todo('defaultChecked를 주입하지 않으면 스위치가 꺼진 상태로 렌더링된다.');
+describe('Switch 컴포넌트', () => {
+  test('checked와 defaultChecked를 주입하지 않으면 스위치가 꺼진 상태로 렌더링된다', () => {
+    render(<Switch />);
+    const input = screen.getByRole('switch') as HTMLInputElement;
+    expect(input.checked).toBe(false);
+  });
 
-test.todo('주입한 defaultChecked로 스위치가 켜진 상태로 렌더링된다.');
+  test('defaultChecked를 주입하면 스위치가 켜진 상태로 렌더링된다', () => {
+    render(<Switch defaultChecked />);
+    const input = screen.getByRole('switch') as HTMLInputElement;
+    expect(input.checked).toBe(true);
+  });
 
-test.todo('checked를 주입하지 않으면 스위치가 꺼진 상태로 렌더링된다.');
+  test('checked를 주입하면 스위치가 켜진다', () => {
+    render(<Switch checked />);
+    const input = screen.getByRole('switch') as HTMLInputElement;
+    expect(input.checked).toBe(true);
+  });
 
-test.todo('checked를 주입하면 스위치가 켜진다.');
+  test('defaultChecked를 주입하더라도 checked를 false로 주입하면 스위치가 꺼진다', () => {
+    render(<Switch defaultChecked checked={false} />);
+    const input = screen.getByRole('switch') as HTMLInputElement;
+    expect(input.checked).toBe(false);
+  });
 
-test.todo('disabled 상태에서는 클릭 이벤트가 발생하지 않는다.');
+  test('disabled 상태에서는 클릭 이벤트가 발생하지 않는다', () => {
+    const handleChange = vi.fn();
+    render(<Switch disabled onChange={handleChange} />);
 
-test.todo('label를 주입하면 라벨이 렌더링된다.');
+    const input = screen.getByRole('switch') as HTMLInputElement;
+    userEvent.click(input);
 
-test.todo('클릭 시 상태가 변경되고 onChange를 호출한다.');
+    expect(handleChange).not.toHaveBeenCalled();
+  });
+
+  test('ref가 올바르게 전달된다', () => {
+    const ref = createRef<HTMLInputElement>();
+    render(<Switch ref={ref} />);
+    expect(ref.current).toBeInstanceOf(HTMLInputElement);
+  });
+
+  test('className을 주입하면 추가로 전달한다.', () => {
+    const customClassName = faker.word.noun();
+    render(<Switch className={customClassName} />);
+    expect(screen.getByRole('switch')).toHaveClass(customClassName);
+  });
+});
