ff85f4b08f846820dc384b2a577687ce49ff11c9
Jeongmin Lee <clcl6084@gmail.com>
2024-12-18 00:57:14 +0900

    chore(vitest): update coverage configuration to exclude specific files and directories

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,25 @@ export default defineConfig({
     globals: true,
     passWithNoTests: true,
     watch: false,
+    coverage: {
+      exclude: [
+        '.templates/*',
+        '.storybook/*',
+        'packages/*/src/*.stories.tsx',
+        'packages/*/src/*.test.*',
+        'packages/*/src/*.d.ts',
+        'packages/*/src/*.ts',
+        'packages/*/src/*/*.ts',
+        
+        'packages/*/.storybook/*',
+        'packages/*/coverage/*',
+        'packages/*/vitest.setup.ts',
+        'packages/*/dist/*',
+
+        'packages/*/*.config.ts',
+        'packages/*/*.d.ts',
+        '*.ts',
+      ],
+    },
   },
 });
