6571ac6be840558846e87356e4bc83a275d3f5d0
Evan <clcl6084@gmail.com>
2024-12-18 23:33:54 +0900

    fix(test): add exclude in root vitest.config.ts (#52)
    * chore(input): add CSS module declaration and remove obsolete type definition file
    
    * chore(vitest): update coverage configuration to exclude specific files and directories
    
    * chore(vitest): refine coverage configuration by adding index.ts and removing unnecessary TypeScript file patterns

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,22 @@ export default defineConfig({
     globals: true,
     passWithNoTests: true,
     watch: false,
+    coverage: {
+      exclude: [
+        '.templates/*',
+        '.storybook/*',
+        'packages/*/src/*.stories.tsx',
+        'packages/*/src/*.test.*',
+        'packages/*/src/index.ts',
+        'packages/*/src/*.d.ts',
+        'packages/*/.storybook/*',
+        'packages/*/coverage/*',
+        'packages/*/vitest.setup.ts',
+        'packages/*/dist/*',
+        'packages/*/*.config.ts',
+        'packages/*/*.d.ts',
+        '*.ts',
+      ],
+    },
   },
 });
