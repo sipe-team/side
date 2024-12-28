db3b987c67b369c387e6b478b00dc0e86e6d67f9
Evan <clcl6084@gmail.com>
2024-12-28 19:16:04 +0900

    feat(card): Card 컴포넌트 구현 (#51)
    * feat(card): Card 컴포넌트 보일러 템플릿 생성
    
    * feat(card): Card 테스트 코드 작성
    
    * feat(card): Card 컴포넌트 구체화
    
    * feat(card): Card 스타일, CSS 수정
    
    * chore(card): border-radius 8px -> 12px
    
    * fix: lock file 수정
    
    * chore(token): cyan300 색상 변경 #67e8f9 -> #00ffff
    
    * fix(card): 오타 수정 및 색상 토큰 적용
    
    * fix(card): border 스타일과 테스트 설명에 컬로 토큰으로 업데이트
    
    * chore: 함수를 객체 매핑으로 수정
    
    - https://github.com/sipe-team/3-1_sds/pull/51#pullrequestreview-2486449540

---
+++ packages/card/src/Card.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Card } from './Card';
+
+const meta = {
+  title: 'Card',
+  component: Card,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Card>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <span style={{ fontSize: '20px' }}>Card</span>,
+    ratio: 'rectangle',
+    variant: 'filled',
+  },
+};
---
+++ packages/card/src/Card.test.tsx
@@ -0,0 +1,52 @@
+import { color } from '@sipe-team/tokens';
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import { Card } from './Card';
+
+test('children으로 넘어간 요소를 반환한다. ', () => {
+  render(
+    <Card>
+      <span>Card</span>
+    </Card>,
+  );
+
+  expect(screen.getByText('Card')).toBeInTheDocument();
+});
+
+test('Card는 default로 padding(20px)을 갖는다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({ padding: '20px' });
+});
+
+test('Card는 default로 요소를 중앙정렬을 한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  });
+});
+
+test('ratio가 default로 aspect-ratio는 16/9로 반환한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle('aspect-ratio: 16 / 9');
+});
+
+test('ratio에 wide로 넣으면 aspect-ratio는 21/9로 반환한다.', () => {
+  render(<Card ratio="wide">Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle('aspect-ratio: 21 / 9');
+});
+
+test('variant는 default로 filled를 적용한다.', () => {
+  render(<Card>Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({
+    border: `1px solid ${color.gray200}`,
+  });
+});
+
+test(`variant가 outline으로 넣으면 border(${color.cyan300}) 색상을 적용한다.`, () => {
+  render(<Card variant="outline">Card</Card>);
+  expect(screen.getByText('Card')).toHaveStyle({
+    border: `1px solid ${color.cyan300}`,
+  });
+});
