f527341de4f785ad3752bc3f226911355da0d6b9
Danny <68424403+kimdaeyeobbb@users.noreply.github.com>
2025-01-09 23:53:36 +0900

    feat(avatar): avatar 컴포넌트 구현 (#45)
    * feat(avatar): implement test
    
    * feat(avatar): scaffold package
    
    * feat(avatar): implement component
    
    * feat(avatar): add avatar story

---
+++ packages/avatar/src/Avatar.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Avatar } from "./Avatar";
+import { faker } from "@faker-js/faker";
+
+const meta = {
+  title: "Avatar",
+  component: Avatar,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Avatar>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const testImage = faker.image.avatar();
+
+export const Basic: Story = {
+  args: {
+    src: "https://randomuser.me/api/portraits/men/1.jpg",
+    alt: "대체 텍스트",
+  },
+};
+
+export const Sizes: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: "1rem" }}>
+      <Avatar size="xs" src={testImage} alt="XSmall" />
+      <Avatar size="sm" src={testImage} alt="small" />
+      <Avatar size="md" src={testImage} alt="medium" />
+      <Avatar size="lg" src={testImage} alt="large" />
+      <Avatar size="xl" src={testImage} alt="XLarge" />
+    </div>
+  ),
+};
+
+export const Shapes: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: "1rem" }}>
+      <Avatar shape="circle" src={testImage} alt="원형" />
+      <Avatar shape="rounded" src={testImage} alt="둥근 사각형" />
+      <Avatar shape="square" src={testImage} alt="사각형" />
+    </div>
+  ),
+};
