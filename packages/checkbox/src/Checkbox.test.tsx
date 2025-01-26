5b74659092a664a7867e7639b96c21ab8de9c34b
kimdaeyeobbb <kimdyk2@hanmail.net>
2025-01-26 23:13:01 +0900

    test(checkbox): implement checkbox unit test

--- packages/checkbox/src/Checkbox.test.tsx
+++ packages/checkbox/src/Checkbox.test.tsx
@@ -1,51 +1,97 @@
-import { describe, test } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { Checkbox } from './Checkbox';
+import { CHECKBOX_SIZES, type CheckboxSize } from './constants/size';
 
 describe('Checkbox 기본 동작 테스트', () => {
-  test.todo('체크박스의 상태를 설정할 수 있다.');
-  test.todo('체크박스의 레이블을 클릭하면 체크박스의 상태가 변경된다.');
-  test.todo('체크박스를 클릭하면 체크박스의 상태가 변경된다.');
+  test('체크박스의 상태를 설정할 수 있다.', () => {
+    render(<Checkbox checked={true} label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    expect(checkbox.checked).toBe(true);
+  });
+
+  test('체크박스의 레이블을 클릭하면 체크박스의 상태가 변경된다.', () => {
+    const handleChange = vi.fn();
+    render(<Checkbox label="Test Checkbox" onCheckedChange={handleChange} />);
+    const label = screen.getByText('Test Checkbox');
+    fireEvent.click(label);
+    expect(handleChange).toHaveBeenCalledWith(true);
+  });
+
+  test('체크박스를 클릭하면 체크박스의 상태가 변경된다.', () => {
+    const handleChange = vi.fn();
+    render(<Checkbox label="Test Checkbox" onCheckedChange={handleChange} />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    fireEvent.click(checkbox);
+    expect(handleChange).toHaveBeenCalledWith(true);
+  });
 });
 
 describe('Checkbox 스타일 테스트', () => {
-  test.todo('전달받은 style을 컴포넌트에 적용한다.');
-  test.todo(
-    'checked를 주입하지 않으면 checkbox가 체크되지 않은 상태로 렌더링된다.',
-  );
-  test.todo('checked를 주입하면 checkbox가 체크된 상태로 렌더링된다.');
-  test.todo(
-    'disabled를 주입하지 않으면 checkbox가 활성화된 상태로 렌더링된다.',
-  );
-  test.todo('disabled를 주입하면 checkbox가 비활성화된 상태로 렌더링된다.');
+  test('전달받은 style을 컴포넌트에 적용한다.', () => {
+    render(<Checkbox label="Test Checkbox" style={{ margin: '10px' }} />);
+    const checkbox = screen.getByLabelText('Test Checkbox').parentElement;
+    expect(checkbox).toHaveStyle('margin: 10px');
+  });
 
-  test.each(['sm', 'md', 'lg'])(
-    'size로 주입한 %s를 checkbox의 크기로 설정한다.',
-    () => {
-      // test code
+  test('checked를 주입하지 않으면 checkbox가 체크되지 않은 상태로 렌더링된다.', () => {
+    render(<Checkbox label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    expect(checkbox.checked).toBe(false);
+  });
+
+  test('checked를 주입하면 checkbox가 체크된 상태로 렌더링된다.', () => {
+    render(<Checkbox checked={true} label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    expect(checkbox.checked).toBe(true);
+  });
+
+  test('disabled를 주입하지 않으면 checkbox가 활성화된 상태로 렌더링된다.', () => {
+    render(<Checkbox label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    expect(checkbox.disabled).toBe(false);
+  });
+
+  test('disabled를 주입하면 checkbox가 비활성화된 상태로 렌더링된다.', () => {
+    render(<Checkbox disabled={true} label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    expect(checkbox.disabled).toBe(true);
+  });
+
+  test.each(['small', 'medium', 'large'])(
+    'size로 %s주입시 해당 크기로 checkbox의 크기를 설정한다.',
+    (size) => {
+      render(<Checkbox size={size as CheckboxSize} label="Test Checkbox" />);
+      const checkbox = screen.getByLabelText('Test Checkbox').parentElement;
+      const expectedSize = CHECKBOX_SIZES[size as CheckboxSize].checkboxSize;
+      expect(checkbox).toHaveStyle(`--checkbox-size: ${expectedSize}px`);
     },
   );
-  test.todo('size를 주입하지 않으면 기본 값 md로 크기를 설정한다.');
+
+  test('size를 주입하지 않으면 기본 값 medium로 크기를 설정한다.', () => {
+    render(<Checkbox label="Test Checkbox" />);
+    const checkbox = screen.getByLabelText('Test Checkbox').parentElement;
+    expect(checkbox).toHaveStyle(
+      `--checkbox-size: ${CHECKBOX_SIZES.medium.checkboxSize}px`,
+    );
+  });
 });
 
 describe('Checkbox 이벤트 테스트', () => {
   // default
-  test.todo('체크박스 영역을 클릭하면 onChange 콜백이 호출된다.');
-
-  // optional
-  test.todo('체크박스 키보드 이벤트 테스트');
-  test.todo('체크박스 마우스 이벤트 테스트');
-  test.todo('체크박스 포커스 이벤트 테스트');
-});
-
-describe('Checkbox asChild 테스트', () => {
-  test.todo(
-    'asChild가 true일 때, children으로 전달된 요소에 Typography 컴포넌트를 적용한다.',
-  );
-
-  test.todo('asChild를 주입하지 않으면 div태그로 요소를 그린다.');
+  test('체크박스 영역을 클릭하면 onChange 콜백이 호출된다.', () => {
+    const handleChange = vi.fn();
+    render(<Checkbox label="Test Checkbox" onCheckedChange={handleChange} />);
+    const checkbox = screen.getByLabelText('Test Checkbox') as HTMLInputElement;
+    fireEvent.click(checkbox);
+    expect(handleChange).toHaveBeenCalledWith(true);
+  });
 });
 
 describe('Checkbox 접근성 테스트', () => {
-  // 스크린 리더 사용자가 체크박스의 용도를 확인할 수 있도록 <label>, title, aria-labelledby 중 1가지 방식을 사용해서 레이블을 제공해야 한다.
-  // 이때 가능하면 <label>을 사용하여 이용자가 레이블을 클릭하였을 때에도 값을 선택할 수 있도록 구현하는게 좋다.
-  test.todo('체크박스에 접근 가능한 레이블이 제공되어야 한다.');
+  test('label을 통해 체크박스의 용도를 제공한다.', () => {
+    render(<Checkbox label="Accessible Checkbox" />);
+    const checkbox = screen.getByLabelText('Accessible Checkbox');
+    expect(checkbox).toBeInTheDocument();
+  });
 });
