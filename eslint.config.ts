4d35f528935b38388c68b551238524db4e610b3b
후니 <jihoon7705@gmail.com>
2025-01-28 22:15:01 +0900

    chore: enforce ternary operator over logical operator (#112)
    * chore: enforce ternary operator over logical operator
    
    * fix: dedupe no-unused-expressions eslint rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,6 +17,11 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
 
       'no-duplicate-imports': 'off',
+      'no-unused-expressions': "off",
+      '@typescript-eslint/no-unused-expressions': ['error', {
+        allowShortCircuit: false,
+        allowTernary: true
+      }]
     },
   },
 
