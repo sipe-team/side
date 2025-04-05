16ec2e0980f0a412854562cb3427784fe38594d0
y09n <euihyun.yang.9x@gmail.com>
2025-04-05 12:45:15 +0900

    fix(avatar): apply vanilla-extract (#143)
    * fix(avatar): apply vanilla-extract
    
    * fix(avatar): type error

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
   dts: true,
+  esbuildPlugins: [vanillaExtractPlugin()],
   format: ['esm', 'cjs'],
-  loader: {
-    '.css': 'local-css',
-  },
 });
