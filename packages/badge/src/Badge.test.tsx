641aaeb97ab280f670456a733f8d64499f73401d
Jeongmin Lee <clcl6084@gmail.com>
2025-04-17 13:58:27 +0900

    test(badge): color token update on test

--- packages/badge/src/Badge.test.tsx
+++ packages/badge/src/Badge.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import { Badge } from './Badge';
+import { color as colorToken } from '@sipe-team/tokens';
 
 test('children으로 입력한 텍스트를 표시한다.', () => {
   render(<Badge>테스트</Badge>);
@@ -14,10 +15,10 @@ test('모서리가 8px radius 형태이다.', () => {
   expect(screen.getByRole('status')).toHaveStyle({ borderRadius: '8px' });
 });
 
-test('글꼴 색상은 teal(#00FFFF)이다.', () => {
+test(`글꼴 색상은 cyan300(${colorToken.cyan300})이다.`, () => {
   render(<Badge>테스트</Badge>);
 
-  expect(screen.getByText('테스트')).toHaveStyle({ color: '#00FFFF' });
+  expect(screen.getByText('테스트')).toHaveStyle({ color: colorToken.cyan300 });
 });
 
 test('글꼴 두께는 semiBold(600)이다.', () => {
@@ -26,28 +27,28 @@ test('글꼴 두께는 semiBold(600)이다.', () => {
   expect(screen.getByText('테스트')).toHaveStyle({ fontWeight: 600 });
 });
 
-test('variant를 주입하지 않으면 filled(배경색 #2D3748)를 기본 형태로 설정한다.', () => {
+test(`variant를 주입하지 않으면 filled(${colorToken.cyan900})를 기본 형태로 설정한다.`, () => {
   render(<Badge>테스트</Badge>);
 
   expect(screen.getByRole('status')).toHaveStyle({
-    backgroundColor: '#2D3748',
+    backgroundColor: colorToken.cyan900,
   });
 });
 
-test('variant가 weak인 경우 배경색 #EDF2F7로 형태를 적용한다.', () => {
+test('variant가 weak인 경우 배경색 gray200로 형태를 적용한다.', () => {
   render(<Badge variant="weak">테스트</Badge>);
 
   expect(screen.getByRole('status')).toHaveStyle({
-    backgroundColor: '#EDF2F7',
+    backgroundColor: '#e4e4e7',
   });
 });
 
-test('variant가 outline인 경우 배경색은 투명, 테두리는 2px 두께의 #2D3748 색상 형태를 적용한다.', () => {
+test('variant가 outline인 경우 배경색은 투명, 테두리는 2px 두께의 cyan900 색상 형태를 적용한다.', () => {
   render(<Badge variant="outline">테스트</Badge>);
 
   expect(screen.getByRole('status')).toHaveStyle({
     backgroundColor: 'transparent',
-    border: '2px solid #2D3748',
+    border: `2px solid ${colorToken.cyan900}`,
   });
 });
 
