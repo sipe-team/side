069abbdd60049e51dc5f4a8764b77d66a08cf36b
danny.yk <danny.yk@kakaocorp.com>
2025-06-30 08:36:16 +0900

    feat(accordion): add test scenario for Accordion component

---
+++ packages/accordion/src/Accordion.test.tsx
@@ -0,0 +1,33 @@
+import { test } from 'vitest';
+
+test.todo('Accordion의 Root에 border-radius를 주입하지 않으면 기본 값 12px으로 border-radius를 설정한다.');
+
+test.todo('Accordion의 Root에 border 옵션을 주입하지 않으면 기본 값 "1px solid #2d3748"로 border를 설정한다.');
+
+test.todo(
+  'Accordion의 Root에 background-color 옵션을 주입하지 않으면 기본 값 "#1a202c"로 background-color를 설정한다.',
+);
+
+test.todo("Accordion의 Root에 padding 옵션을 주입하지 않으면 기본 값 '20px'로 padding을 설정한다.");
+
+test.todo('Accordion의 Trigger에 font-weight를 주입하지 않으면 기본 값 700으로 font-weight를 설정한다.');
+
+test.todo('Accordion의 Trigger에 font-size를 주입하지 않으면 기본 값 14px으로 font-size를 설정한다.');
+
+test.todo('Accordion의 Trigger에 color를 주입하지 않으면 기본 값 #ffffff으로 color를 설정한다.');
+
+test.todo('Accordion의 Trigger에 AccordionArrowIcon이 포함되어 있다.');
+
+test.todo('Accordion의 Body에 border-radius를 주입하지 않으면 기본 값 8px으로 border-radius를 설정한다.');
+
+test.todo('Accordion의 Body에 background-color를 주입하지 않으면 기본 값 #2d3748으로 background-color를 설정한다.');
+
+test.todo('Accordion의 Content에 color를 주입하지 않으면 기본 값 #e4e4e7으로 color를 설정한다.');
+
+test.todo('Accordion의 Content에 font-size를 주입하지 않으면 기본 값 14px으로 font-size를 설정한다.');
+
+test.todo('Accordion의 Content에 font-weight를 주입하지 않으면 기본 값 600으로 font-weight를 설정한다.');
+
+test.todo('Accordion의 Content에 padding을 주입하지 않으면 기본 값 12px 16px으로 padding을 설정한다.');
+
+test.todo('asChild를 주입하지 않으면 p태그로 요소를 그린다.');
