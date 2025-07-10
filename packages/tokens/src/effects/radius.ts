6cc545f14a1502fa419b252793d3762e6215b82e
Jeongmin Lee <clcl6084@gmail.com>
2025-07-10 13:15:35 +0900

    chore(tokens): update radius values to include units

--- packages/tokens/src/effects/radius.ts
+++ packages/tokens/src/effects/radius.ts
@@ -1,10 +1,10 @@
 export const radius = {
-  none: 0,
-  sm: 2,
-  md: 4,
-  lg: 8,
-  xl: 12,
-  full: 9999,
+  none: '0',
+  sm: '2px',
+  md: '4px',
+  lg: '8px',
+  xl: '12px',
+  full: '9999px',
 } as const;
 
 export type Radius = keyof typeof radius;
