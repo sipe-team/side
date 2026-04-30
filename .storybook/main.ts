7493fe88d48eb567dc6a10c3ad7e0b6ef475d124
Evan <clcl6084@gmail.com>
2026-05-01 00:25:02 +0900

    chore(storybook): update config for v10
    - main.ts: use staticDirs instead of CLI --static-dir flag
      add providerImportSource for pnpm MDX shim path resolution
      update addons array (essentials/interactions/links now in core)
    - manager.ts: update imports from @storybook/* to storybook/* subpaths
      (manager-api and theming consolidated into storybook package)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,23 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 export default {
   stories: ['../docs/*.mdx', '../packages/**/*.mdx', '../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-links'],
+  addons: [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            providerImportSource: '@storybook/addon-docs/mdx-react-shim',
+          },
+        },
+      },
+    },
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  staticDirs: ['../public'],
   managerHead: (head) =>
     `${head}
 <link rel="shortcut icon" href="./favicon.ico" type="image/ico">`,
